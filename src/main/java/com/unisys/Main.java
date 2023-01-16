7ff1630d344b9807cdf12b8407a781d14af4c28a
NHC <natalihc_92@hotmail.com>
2023-01-16 18:16:12 +0100

    Se crea clase Ejercicio1. Incluye atributos de distintos tipos de variables y el método suma. En main se crea un objeto Ejercicio1 y se utiliza el método suma de la clase.

--- src/main/java/com/unisys/Main.java
+++ src/main/java/com/unisys/Main.java
@@ -1,8 +1,14 @@
 package com.unisys;
 
+import com.unisys.ejercicios.Ejercicio1;
+
 public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
+        Ejercicio1 objetoEjercicio1 = new Ejercicio1(10,(short)-1,1234567,true,12.34,56.78f);
+        System.out.printf("Suma="+objetoEjercicio1.suma());
     }
+
+
 }
---
+++ src/main/java/com/unisys/ejercicios/Ejercicio1.java
@@ -0,0 +1,24 @@
+package com.unisys.ejercicios;
+
+public class Ejercicio1 {
+    int numero;
+    short marianico;
+    long gasol;
+    boolean independencia;
+    double presupuesto;
+    float longitud;
+    public Ejercicio1(int num, short mari, long gas, boolean indep, double presu, float longit)
+    {
+        numero=num;
+        marianico=mari;
+        gasol=gas;
+        independencia=indep;
+        presupuesto=presu;
+        longitud=longit;
+    }
+    public double suma ()
+    {
+        return gasol+marianico;
+    }
+
+}
